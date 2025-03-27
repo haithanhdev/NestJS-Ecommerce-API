1f1c61f14bc7b61ff90bdb0e5f6dbd00359ba0a4
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-28 01:20:56 +0700

    Send email with template using production mode

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from 'src/shared/config'
-
+import fs from 'fs'
+import path from 'path'
 @Injectable()
 export class EmailService {
   private resend: Resend
@@ -9,12 +10,17 @@ export class EmailService {
     this.resend = new Resend(envConfig.RESEND_API_KEY)
   }
   async sendOTP(payload: { email: string; code: string }) {
+    const subject = 'Mã OTP'
+    const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), {
+      encoding: 'utf-8',
+    })
+
     return await this.resend.emails.send({
       //Chưa verify domain thì để mặc định vậy
-      from: 'Ecommerce admin <onboarding@resend.dev>',
-      to: ['edu.haithanh.nguyen1001@gmail.com'],
-      subject: `Mã OTP`,
-      html: `<strong>${payload.code}</strong>`,
+      from: 'Nest.js Ecommerce <no-reply@devhueit.io.vn>',
+      to: [payload.email],
+      subject: subject,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code),
     })
   }
 }
