cccf0e0706bcf5bd57e3c250838b2d75ba3da4ef
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-27 16:54:42 +0700

    Serialize data response when user register successfully

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
-import { RegisterBodyDTO } from 'src/routes/auth/auth.dto'
+import { ZodSerializerDto } from 'nestjs-zod'
+import { RegisterBodyDTO, RegisterResDTO } from 'src/routes/auth/auth.dto'
 
 import { AuthService } from 'src/routes/auth/auth.service'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
   @Post('register')
+  @ZodSerializerDto(RegisterResDTO)
   async register(@Body() body: RegisterBodyDTO) {
     return await this.authService.register(body)
   }
--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,5 +1,24 @@
+import { UserStatus } from '@prisma/client'
+import { create } from 'domain'
 import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
+
+//Không có strict thì nếu data trả về dư thì lược bỏ mà không báo lỗi
+//Có strict mà data trả về bị dư thì báo lỗi liền. Tuy nhiên vẫn thêm dữ liệu vào thành công chỉ lỗi bước trả về thôi
+const UserSchema = z.object({
+  id: z.number(),
+  email: z.string(),
+  name: z.string(),
+  phoneNumber: z.string(),
+  avatar: z.string().nullable(),
+  status: z.enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.BLOCKED]),
+  roleId: z.number(),
+  createdById: z.number().nullable(),
+  updatedById: z.number().nullable(),
+  deletedAt: z.date().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+})
 
 const RegisterBodySchema = z
   .object({
@@ -21,3 +40,5 @@ const RegisterBodySchema = z
   })
 
 export class RegisterBodyDTO extends createZodDto(RegisterBodySchema) {}
+
+export class RegisterResDTO extends createZodDto(UserSchema) {}
