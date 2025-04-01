f4486a4ceed30127b22497df3bad12c135c4764b
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-04-02 00:33:43 +0700

    Script auto delete and create permissions

---
+++ initialScript/create-permissions.ts
@@ -0,0 +1,77 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from 'src/app.module'
+import { HTTPMethod } from 'src/shared/constants/role.constants'
+import { PrismaService } from 'src/shared/services/prisma.service'
+
+const prisma = new PrismaService()
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
+
+  const server = app.getHttpServer()
+  const router = server._events.request._router
+  const permissionsInDb = await prisma.permission.findMany({
+    where: {
+      deletedAt: null,
+    },
+  })
+  const availableRoutes: { path: string; method: keyof typeof HTTPMethod; name: string }[] = router.stack
+    .filter((layer) => layer.route)
+    .map((layer) => {
+      const path = layer.route.path
+      const method = Object.keys(layer.route.methods)[0].toUpperCase() as keyof typeof HTTPMethod
+      return {
+        path,
+        method,
+        name: method + ' ' + path,
+      }
+    })
+  //Tạo object permissionInDbMap với key là [method-path]
+  const permissionsInDbMap: Record<string, (typeof permissionsInDb)[0]> = permissionsInDb.reduce((acc, item) => {
+    acc[`${item.method}-${item.path}`] = item
+    return acc
+  }, {})
+  //Tao object availableRoutesMap với key là [method-path]
+  const availableRoutesMap: Record<string, (typeof availableRoutes)[0]> = availableRoutes.reduce((acc, item) => {
+    acc[`${item.method}-${item.path}`] = item
+    return acc
+  }, {})
+  //Tim permission trong database ma khong ton tai trong availableRoutes
+  const permissionsToDelete = permissionsInDb.filter((item) => {
+    return !availableRoutesMap[`${item.method}-${item.path}`]
+  })
+  if (permissionsToDelete.length > 0) {
+    const deleteResult = await prisma.permission.deleteMany({
+      where: {
+        id: {
+          in: permissionsToDelete.map((item) => item.id),
+        },
+      },
+    })
+    console.log('Deleted permissions', deleteResult.count)
+  } else {
+    console.log('No permissions to delete')
+  }
+  //Tim routes ma khong ton tai trong permissionsInDb
+  const routesToAdd = availableRoutes.filter((item) => {
+    return !permissionsInDbMap[`${item.method}-${item.path}`]
+  })
+  //Them routes moi vao database
+  if (routesToAdd.length > 0) {
+    const createResult = await prisma.permission.createMany({
+      data: routesToAdd,
+      skipDuplicates: true,
+    })
+    console.log('Created permissions', createResult.count)
+  } else {
+    console.log('No permissions to add')
+  }
+  //Xoa permission trong database
+  //Them vao DB
+  //   const result = await prisma.permission.createMany({
+  //     data: availableRoutes,
+  //     skipDuplicates: true,
+  //   })
+  process.exit(0)
+}
+bootstrap()
