3d0a9e3b0c8954db1aada562e829a4da5faa9fe5
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-04-26 10:46:32 +0700

    Fixbug cartItem

--- initialScript/create-permissions.ts
+++ initialScript/create-permissions.ts
@@ -3,7 +3,7 @@ import { AppModule } from 'src/app.module'
 import { HTTPMethod, RoleName } from 'src/shared/constants/role.constants'
 import { PrismaService } from 'src/shared/services/prisma.service'
 
-const SellerModule = ['AUTH', 'MEDIA', 'MANAGE-PRODUCT', 'PRODUCT-TRANSLATION', 'PROFILE', 'CART']
+const SellerModule = ['AUTH', 'MEDIA', 'MANAGE-PRODUCT', 'PRODUCT-TRANSLATIONS', 'PROFILE', 'CART']
 const ClientModule = ['AUTH', 'MEDIA', 'PROFILE', 'CART']
 
 const prisma = new PrismaService()
