588d9f3b66165576157a1c3b4841528ae5198a83
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-04-30 18:02:55 +0700

    Change API Key Guard

--- src/shared/guards/payment-api-key.guard.ts
+++ src/shared/guards/payment-api-key.guard.ts
@@ -4,7 +4,7 @@ import envConfig from 'src/shared/config'
 export class PaymentAPIKeyGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest()
-    const xAPIKey = request.headers['payment-api-key']
+    const xAPIKey = request.headers['Authorization']?.split(' ')[1]
     if (xAPIKey !== envConfig.PAYMENT_API_KEY) {
       throw new UnauthorizedException()
     }
