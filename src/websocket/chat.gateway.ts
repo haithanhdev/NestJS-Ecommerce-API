7be8075589e9bb2f88babacc9001db16a8f7716b
realhaidinh <dinhnguyenminhhai@gmail.com>
2025-05-19 01:31:59 +0700

    fix chat session logic

--- src/websocket/chat.gateway.ts
+++ src/websocket/chat.gateway.ts
@@ -10,25 +10,47 @@ import { SharedWebsocketRepository } from 'src/shared/repositories/shared-websoc
 export class ChatGateway {
   @WebSocketServer()
   server: Server
+  private sessions = new Map<string, Set<string>>()
+  private chatMap = new Map<string, string>()
+  constructor(private readonly chatService: ChatService) { }
+  @SubscribeMessage('start-chat')
+  async handleStartChat(@ConnectedSocket() client: Socket, @MessageBody() data: { toUserId: number }) {
+    const fromUserId: number = client.data.userId
+    const socketId: string = client.id
+    const roomKey = generateRoomKey(fromUserId, data.toUserId)
     
-  constructor(private readonly chatService: ChatService,
-    private readonly sharedWebsocketRepository: SharedWebsocketRepository) { }
-  
+    this.chatMap.set(socketId, roomKey)
+
+    if (!this.sessions.has(roomKey)) {
+      this.sessions.set(roomKey, new Set())
+    }
+    
+    const targets = this.sessions.get(roomKey);
+
+    targets?.add(socketId);
+  }
   @SubscribeMessage('send-message')
   async handleEvent(@ConnectedSocket() client: Socket, @MessageBody() newMessage: CreateMessageBodyType) {
-    newMessage.fromUserId = client.data.userId
+    const {userId} = client.data
+    newMessage.fromUserId = userId
     const result = await this.chatService.create(newMessage)
     try {
-      const sockets = await Promise.all([
-        this.sharedWebsocketRepository.findMany(newMessage.toUserId),
-        this.sharedWebsocketRepository.findMany(newMessage.fromUserId)
-      ])
-      sockets.flat().forEach((ws) => {
-        this.server.to(ws.id).emit('receive-message', result)
+      const roomKey = generateRoomKey(userId, newMessage.toUserId)
+      const sockets = this.sessions.get(roomKey)
+      sockets?.forEach((id) => {
+        this.server.to(id).emit('receive-message', result)
       })
-      
+
     } catch (error) {
       Logger.log(error)
     }
   }
+  async handleDisconnect(client: Socket) {
+    const roomKey = this.chatMap.get(client.id)
+    this.sessions.get(roomKey as string)?.delete(client.id)
+  }
+}
+
+function generateRoomKey(userA: number, userB: number): string {
+  return [userA, userB].sort((a, b) => a - b).join('-');
 }
