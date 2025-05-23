9a6ec46137e5dc38abef7dabc63f291b4f076237
Hai Thanh <haithanh.nguyen@estuary.solutions>
2025-05-24 02:24:04 +0700

    Feat: OAuth Google

--- src/shared/models/shared-user.model.ts
+++ src/shared/models/shared-user.model.ts
@@ -24,16 +24,27 @@ export const UserSchema = z.object({
 /**
  * Áp dụng cho Response của api GET('profile') và GET('users/:userId')
  */
-export const GetUserProfileResSchema = UserSchema.omit({
-  password: true,
-  totpSecret: true,
-  phoneNumber: true,
-}).extend({
-  role: RoleSchema.pick({
-    id: true,
-    name: true,
+export const GetUserProfileResSchema = z
+  .object({
+    id: z.number(),
+    email: z.string().email(),
+    name: z.string().min(1).max(100),
+    phoneNumber: z.string().optional(),
+    avatar: z.string().nullable(),
+    status: z.enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.BLOCKED]),
+    roleId: z.number().positive(),
+    createdById: z.number().nullable(),
+    updatedById: z.number().nullable(),
+    deletedById: z.number().nullable(),
+    deletedAt: z.date().nullable(),
+    createdAt: z.date(),
+    updatedAt: z.date(),
   })
-    .extend({
+  .extend({
+    role: RoleSchema.pick({
+      id: true,
+      name: true,
+    }).extend({
       permissions: z.array(
         PermissionSchema.pick({
           id: true,
@@ -43,11 +54,8 @@ export const GetUserProfileResSchema = UserSchema.omit({
           method: true,
         }),
       ),
-    })
-    .extend({
-      phoneNumber: z.string().min(9).max(15).optional(),
     }),
-})
+  })
 
 /**
  * Áp dụng cho Response của api PUT('profile') và PUT('users/:userId')
