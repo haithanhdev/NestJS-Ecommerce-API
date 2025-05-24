500b10c30a3dcd54546054866068021cd77abc51
Hai Thanh <haithanh.nguyen@estuary.solutions>
2025-05-24 16:10:09 +0700

    Add: Default value phoneNumber

--- src/shared/models/shared-user.model.ts
+++ src/shared/models/shared-user.model.ts
@@ -8,7 +8,7 @@ export const UserSchema = z.object({
   email: z.string().email(),
   name: z.string().min(1).max(100),
   password: z.string().min(6).max(100),
-  phoneNumber: z.string().min(9).max(15),
+  phoneNumber: z.string().min(9).max(15).default('000000000'),
   avatar: z.string().nullable(),
   totpSecret: z.string().nullable(),
   status: z.enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.BLOCKED]),
